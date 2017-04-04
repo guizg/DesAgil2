b022829da918e28df5d934f86dfb247439e4df7a
guizg <guizgraicer@gmail.com>
2017-04-04 11:12:34 -0300

    atualizei a versao do hashi

--- desagil-lucianogic-0.3/src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/Emitter.java
+++
@@ -1,5 +0,0 @@
-package br.pro.hashi.ensino.desagil.lucianogic.model;
-
-public interface Emitter {
-	public boolean read();
-}
--- desagil-lucianogic-0.3/src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/Receiver.java
+++
@@ -1,5 +0,0 @@
-package br.pro.hashi.ensino.desagil.lucianogic.model;
-
-public interface Receiver {
-	public void connect(Emitter emitter, int index);
-}
